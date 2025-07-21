523b55f913d0dfa5c5957a5b07e0b90d2961fd15
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2025-07-21 17:30:03 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'vuejs-template',
+    }
+  },
+  methods: {
+    greeting () {
+      return 'Hello World'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
