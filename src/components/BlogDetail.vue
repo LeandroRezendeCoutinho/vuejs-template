6f6775d98de8b909809290904145dc449e2d18ab
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2025-08-19 08:52:08 -0300

    using route params

---
+++ src/components/BlogDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="detail">
+    <h2>{{ blogPost.title }}</h2>
+    <article>{{ blogPost.body }}</article>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      blogPost: {}
+    }
+  },
+  created() {
+    this.$http.get(`https://jsonplaceholder.typicode.com/posts/${this.id}`)
+      .then(response => {
+        this.blogPost = response.body;
+      })
+      .catch(error => {
+        console.error('Error fetching blog post:', error);
+      });
+  }
+}
+</script>
+
+<style></style>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import ShowBlogs from "./components/ShowBlogs.vue"
 import AddBlog from "./components/AddBlog.vue"
 import All from "./components/All.vue"
+import BlogDetail from "./components/BlogDetail.vue"
 
 export default [
     { path: '/', component: All },
     { path: '/blogs', component: ShowBlogs },
-    { path: '/add', component: AddBlog }
+    { path: '/add', component: AddBlog },
+    { path: '/blog/:id', component: BlogDetail }
 ]
